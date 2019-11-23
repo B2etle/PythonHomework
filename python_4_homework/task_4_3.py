23ed5284c5ec64291c6d77aede0f14418d484ee8
Artsiom <a.zhuk.inc@hotmail.com>
2019-11-23 16:04:54 +0300

    Modify Task 4.3
    Modify the work with the path in the Task 4.3

--- python_4_homework/task_4_3.py
+++ python_4_homework/task_4_3.py
@@ -1,5 +1,6 @@
-from csv import DictReader
-from csv import DictWriter
+import os
+from csv import DictReader, DictWriter
+from pathlib import Path
 from typing import List
 
 
@@ -8,7 +9,6 @@ def get_top_performers(file_path: str, number_of_top_students: int = 5) -> List[
     with open(file_path, 'r', newline='') as file_students:
         students_reader = DictReader(file_students, delimiter=',')
         students = sorted(students_reader, key=lambda student: (float(student['average mark'])), reverse=True)
-
         return [student['student name'] for student in students[:number_of_top_students]]
 
 
@@ -17,8 +17,10 @@ def order_students_by_age(file_path: str):
     with open(file_path, 'r', newline='') as read_students:
         students_reader = DictReader(read_students, delimiter=',')
         students = sorted(students_reader, key=lambda student: (int(student['age'])), reverse=True)
-
-    with open(file_path[:len(file_path)-4] + '_order_by_age.csv', 'w', newline='') as write_students:
+    file_name = f'{os.path.dirname(file_path)}/{Path(file_path).resolve().stem}'
+    with open(f'{file_name}_order_by_age.csv', 'w', newline='') as write_students:
         students_writer = DictWriter(write_students, students_reader.fieldnames)
         students_writer.writeheader()
         students_writer.writerows(students)
+
+order_students_by_age('/home/tamara/Documents/WorkSpace/Trainings/Introduction_to_Python/Python/PythonHomework/python_4_homework/students.csv')
